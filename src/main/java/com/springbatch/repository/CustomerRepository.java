5bac6d308949deaf13b89817fe4d44fe530bffec
Deep-Inexture <deeppanchal@inexture.com>
2024-05-20 11:39:57 +0530

    Initial Commit

---
+++ src/main/java/com/springbatch/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.springbatch.repository;
+
+import com.springbatch.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.io.Serializable;
+
+public interface CustomerRepository extends JpaRepository<Customer, Serializable> {
+}
